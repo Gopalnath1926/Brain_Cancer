c14bd563869cec26c3dbb11d8fd4df494c4d1436
Gopal Nath <46755110+Gopalnath1926@users.noreply.github.com>
2022-06-26 12:49:12 -0500

    Update app_BCNS.py

--- app_BCNS.py
+++ app_BCNS.py
@@ -44,7 +44,7 @@ def main():
 
     html_temp = """
     <div style ="background-color:turquoise;padding:9px">
-    <h1 style ="color:black;text-align:center;">WBP tool for brain cancer survival</h1>
+    <h1 style ="color:black;text-align:center;">WBP tool for brain cancer survival (Prototype)</h1>
     </div>
     """
 
